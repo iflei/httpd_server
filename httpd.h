f39a286c2818e51cfd34a0c758b145063ac89907
iflei <admin@iflei.com>
2017-07-26 10:46:18 +0800

    httpd.h

---
+++ httpd.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define SIZE 8192
+
+enum LEVEL
+{
+	SUCCESS,
+	NOTICE,
+	WARNING,
+	ERROR,
+	FATAL,
+};
+
+int startup(int port);
+void print_log(char* error, int level);
+int request_handle(int sock);
